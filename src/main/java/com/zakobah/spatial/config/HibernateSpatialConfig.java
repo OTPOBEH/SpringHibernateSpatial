2828beac7260c6a84ae4cb253be8ec92a0510f24
3akobah <3akobah@gmail.com>
2019-04-10 21:03:06 +0300

    Initial commit

---
+++ src/main/java/com/zakobah/spatial/config/HibernateSpatialConfig.java
@@ -0,0 +1,55 @@
+package com.zakobah.spatial.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+import java.util.Properties;
+
+@Configuration
+@EnableTransactionManagement
+@PropertySource("application.properties")
+public class HibernateSpatialConfig {
+    private String url, username, password;
+
+    @Autowired
+    public HibernateSpatialConfig(Environment env) {
+        url = env.getProperty("database.url");
+        username = env.getProperty("database.username");
+        password = env.getProperty("database.password");
+    }
+
+    @Bean
+    public LocalSessionFactoryBean sessionFactory() {
+        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+        sessionFactory.setDataSource(dataSource());
+        sessionFactory.setPackagesToScan("com.zakobah.spatial.models");
+        sessionFactory.setHibernateProperties(hibernateProperties());
+
+        return sessionFactory;
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        dataSource.setUrl(url);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+
+        return dataSource;
+    }
+
+    private Properties hibernateProperties() {
+        Properties hibernateProperties = new Properties();
+        hibernateProperties.setProperty("hibernate.dialect", "org.hibernate.spatial.dialect.mysql.MySQL56SpatialDialect");//TODO: Да кажа на другите да променят диалекта!!!
+
+        return hibernateProperties;
+    }
+}
